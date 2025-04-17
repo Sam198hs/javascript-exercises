7521b5ceb75fd53767b2e6a71d734903ad8334fc
Sam198Hs <sam198hs@gmail.com>
2025-04-17 11:38:33 +0530

    Updated the excerices

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const getAge = (person) => {
+      const deathYear = person.yearOfDeath || new Date().getFullYear();
+      return deathYear - person.yearOfBirth;
+    };
+  
+    return people.reduce((oldest, current) => {
+      return getAge(current) > getAge(oldest) ? current : oldest;
+    });
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
